2a86a5965cc5d4cd28df49f1270f8a6a411ad1ae
seunghyeonKang <number42isuser@gmail.com>
2024-11-19 08:41:57 +0900

    Fix: 폰트 수정

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,9 @@
 import { Link } from "react-router-dom";
 import { styled } from "styled-components";
-import font from "styles/font";
 
 const Text = styled.p`
   margin: 0;
-  ${() => font.regular_20}
+  font-size: 2rem;
 `;
 
 export default function NotFound() {
